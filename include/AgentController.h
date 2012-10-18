7b235d5a60860c5036bf72464f727dff08bb13b6
RedSpaceman <samuel.corcoran@gmail.com>
2012-10-18 16:54:07 +0200

    Removed those damn m_ prefixes. Yuck.

--- include/AgentController.h
+++ include/AgentController.h
@@ -23,14 +23,14 @@ class AgentController {
 	void getNearbyAgents(list<Agent*> &nearbyAgents, Vec3f centreLocation, float radius);
 	int convertLocToBin(Vec3f &loc);
 
-	list<Agent>	mAgents;
+	list<Agent>	agents;
 
 	// Spatial Binning variables
-	vector<SpatialBin> * m_bins;
+	vector<SpatialBin> * gridBins;
 
-	int m_binSize;
-	int m_xBins;
-	int m_yBins;
-	int m_totalBins;
+	int binSize;
+	int xBins;
+	int yBins;
+	int totalBins;
 	
 };
